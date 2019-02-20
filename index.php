cd37168a1ced3d15b72a186b6148f4586910e283
gifailes <47746732+gifailes@users.noreply.github.com>
2019-02-20 14:06:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -53,46 +53,65 @@
         ?>
         <br>
         <?php 
-            $n1=24;
-            $n2=4;
-            
-            if($n1>$n2){
-                echo "O maior número é " . $n1;
-            }
-            else{
-                echo "O maior número é " . $n2;
-            }
-        ?>
-        <br>
-        <?php 
-            $n1=24;
-            $n2=4;
-            $n3=56;
-            
-            if(($n1>$n2)&&($n1>$n3)){
-                echo "O maior número é " . $n1;
-            }
-            elseif(($n2>$n1)&&($n2>$n3)){
-                echo "O maior número é " . $n2;
-            }
-            else{
-                echo "O maior número é " . $n3;
-            }
-        ?>
-        <br>
-        <?php 
+        
             $n1=4;
-            $n2=4;
-            
+            $n2=8;
             if($n1>$n2){
-                echo "O maior número é " . $n1;
-            }
-            elseif($n2>$n1){
-                echo "O maior número é " . $n2;
-            }
-            else{
-                echo "São iguais";
+                echo "O maior número é: " . $n1;
+            } elseif ($n2>$n1){
+                echo "O maior número é: " . $n2;
             }
         ?>
+        <br>
+        <?php
+            $n1=4;
+            $n2=89;
+            $n3=45;
+            if(($n1>$n2)&&($n1>$n3)){
+                echo "O maior número é: " . $n1;
+            } elseif(($n2>$n3)&&($n2>$n1)) {
+                echo "O maior número é: " . $n2;
+            } elseif(($n3>$n1)&&($n3>$n2)) {
+                echo "O maior número é: " . $n3;
+            }
+        ?>
+        <br>
+        <?php
+            $n1=8;
+            $n2=8;
+            if($n1>$n2){
+                echo "O maior número é: " . $n1;
+            } elseif ($n2>$n1){
+                echo "O maior número é: " . $n2;
+            } elseif($n1==$n2){
+                echo "Os numeros são iguais";
+            }
+        ?>
+        <br>
+        <?php
+            $idade=15;
+            
+            if($idade<=18){
+                echo "Meia entrada";
+            } elseif ($idade>=60){
+                echo "Meia entrada";
+            } else {
+                echo "Inteira";
+            }
+        ?>
+        <br>
+        <?php
+            $j=1;
+            while($j<11){
+                $X=0;
+                for($i = $X; $i < 11; $i++) {
+                echo "$j x $i=";
+                echo $j*$i;
+                echo "<br>";
+                $X++ ;
+                }echo "<br>";
+            $j++;
+            }
+            ?>
     </body>
 </html>
