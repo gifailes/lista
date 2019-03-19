cbcf2ef7cb1bf10afa2993f885b960ead60d48cd
gifailes <47746732+gifailes@users.noreply.github.com>
2019-03-19 11:33:12 -0300

    Create questao2.php

---
+++ questao2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css.css" rel="stylesheet">
+        <title>Ola Mundo</title>
+    </head>
+    <body>
+        <form method="POST" action="recebe.php">
+            Número 1: <input type="number" name="n1">
+            Número 2: <input type="number" name="n2">
+            Número 3: <input type="number" name="n3">
+            Número 4: <input type="number" name="n4">
+            Número 5: <input type="number" name="n5">
+            <button type="submit" value="Vai"> Enviar </button>
+        </form>
+    </body>
+</html>
