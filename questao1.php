64285d9ee71c206f6db682072a37df0183aabe0b
gifailes <47746732+gifailes@users.noreply.github.com>
2019-03-19 11:32:17 -0300

    Create questao1.php

---
+++ questao1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css.css" rel="stylesheet">
+        <title>Ola Mundo</title>
+    </head>
+    <body>
+        <a href="questao1.php"> Questão 1</a>
+        <a href="questao2.php"> Questão 2</a>
+        <a href="questao3.php"> Questão 3</a>
+        <a href="questao4.php"> Questão 4</a>
+        <form method="POST" action="soma.php">
+            Número 1: <input type="number" name="num1">
+            Número 2: <input type="number" name="num2">
+            <button type="submit" value="Vai"> Enviar </button>
+        </form>
+    </body>
+</html>
