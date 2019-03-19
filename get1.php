ecad3d480061b4cf6790bb06fae628239b70d620
gifailes <47746732+gifailes@users.noreply.github.com>
2019-03-19 12:22:23 -0300

    Create get1.php

---
+++ get1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ola Mundo</title>
+    </head>
+    <body>
+        <a href="recebeget.php?v1=10&v2=12&v3=23&v4=32&v5=5">Enviar</a>
+    </body>
+</html>
